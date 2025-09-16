1455172cbd4fd752eab5aa4fd01370f7ddc63221
MrLampy <nutchanon.gust@gmail.com>
2025-09-16 23:59:47 +0700

    cell03, ex00

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+user = int(input('Enter a nuber less than 25\n'))
+if user < 25:
+    while user <= 25:
+        print(f'Inside the loop, my variable is {user}')
+        user += 1
+else: print('Error')
